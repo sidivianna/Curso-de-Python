f5207ef3e444975811dcb9d610e99d035e5359ca
Sidinei Vianna <sidineirochavianna19@gmail.com>
2022-05-23 09:05:47 -0300

    Add files via upload

---
+++ cicloforrange.py
@@ -0,0 +1,6 @@
+'''O cliclo for é um ciclo que percorre uma determinada sequência a partir do uso de uma variável qualquer.'''
+
+n = 10
+for cont in range (n):
+    print(cont)
+    
---
+++ operadoreslogicos.py
@@ -0,0 +1,11 @@
+'''and, or, not'''
+
+lado1 = int(input("Digite o primeiro lado: "))
+lado2 = int(input("Digite o segundo lado: "))
+lado3 = int(input("Digite o terceiro lado: "))
+
+if lado1**2 + lado2**2 == lado3**2 or lado3**2 + lado1**2 == lado2**2 or lado2**2 + lado3**2 == lado1**2:
+    print("O triângulo é retângulo.")
+else: 
+    print("O triângulo não é retângulo.")
+    
